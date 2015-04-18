3607fbd7b2ad9df0900b10c118fa7536be3cf987
monk1229 <mam1681@rit.edu>
2015-04-18 14:26:34 -0400

    File array
    parsed arguments to file array

--- src/main.py
+++ src/main.py
@@ -1,7 +1,26 @@
 import pygments
-
 from pygments.lexers import guess_lexer, guess_lexer_for_filename
 
+import sys
+import argparse
+from argparse import ArgumentParser
+
+def filesArray():
+	# make the parser
+	parser = argparse.ArgumentParser(description = "Make array of files")
+	
+	# take command line arguments and make array
+	parser.add_argument('--files', nargs = '*',
+						help = "File names for the array")
+	print(parser.parse_args())
+	#print(parser)
+
+def main():
+	filesArray()
+
+
+
+'''
 def main():
     print("Hello world")
 
@@ -12,7 +31,7 @@ def main():
 
     print(guess_lexer_for_filename('admin_user.rb', file_text))
     #hello world 
-
+'''
 
 if __name__=='__main__':
     main()
