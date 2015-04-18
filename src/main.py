18d87e70ce299cc8ee40cfbbeb586412b2cf3964
Andres Ruiz <andresruiz2010@gmail.com>
2015-04-18 13:03:36 -0400

    Add main file, with some test code

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import pygments
+
+from pygments.lexers import guess_lexer, guess_lexer_for_filename
+
+def main():
+    print("Hello world")
+
+    file_text = ""
+
+    for line in open("examples/admin_user.rb"):
+        file_text += line
+
+    print(guess_lexer_for_filename('admin_user.rb', file_text))
+
+
+if __name__=='__main__':
+    main()
