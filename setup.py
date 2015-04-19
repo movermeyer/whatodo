8ad20fbbaf9e3ac746d9d97c09cc1e98d21dfc59
Andres Ruiz <andresruiz2010@gmail.com>
2015-04-19 12:19:15 -0400

    Update setup.py, bump version number

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0a2',
+    version='0.1.0a3',
 
     description='Python app to help developers manage development tasks using TODO\'s',
     long_description=long_description,
@@ -55,8 +55,6 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
